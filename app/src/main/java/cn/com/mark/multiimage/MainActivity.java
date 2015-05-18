1737c4f1092c712fea469bd8bb4f23b4298818f5
mazhenjin <mazhenjin@sina.com>
2015-05-18 11:15:35 +0800

    test ssh

--- app/src/main/java/cn/com/mark/multiimage/MainActivity.java
+++ app/src/main/java/cn/com/mark/multiimage/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mTextView = (TextView)findViewById(R.id.text);
     }
 
