c5734b4c4076ba35a4ffa2f5533a7dae7d47a1a9
mazhenjin <mazhenjin@sina.com>
2015-05-28 16:56:37 +0800

    modify

--- app/src/main/java/cn/com/mark/multiimage/MainActivity.java
+++ app/src/main/java/cn/com/mark/multiimage/MainActivity.java
@@ -5,69 +5,42 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
 import cn.com.mark.multiimage.core.ImageMainActivity;
 
 
 public class MainActivity extends ActionBarActivity {
-
     private TextView mTextView;
+    private final static int REQUESTCODE = 10010;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
         mTextView = (TextView)findViewById(R.id.text);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_images) {
-            Intent intent = new Intent(MainActivity.this, ImageMainActivity.class);
-            intent.putExtra("action-original", true);
-            startActivityForResult(intent, 10086);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onBtnGetClick(View view){
+        Intent intent = new Intent(MainActivity.this, ImageMainActivity.class);
+        intent.putExtra("action-original", true);
+        startActivityForResult(intent, REQUESTCODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if(resultCode== Activity.RESULT_OK){
-            if(requestCode == 10086){
+            if(requestCode == REQUESTCODE){
                 String paths = "";
-
                 ArrayList<Uri> images = data.getParcelableArrayListExtra("result");
                 for(int i=0;i<images.size();i++){
-                    paths += i+", "+images.get(i).getPath()+"\n\n";
+                    paths += images.get(i).getPath()+"\n\n";
                 }
-
                 mTextView.setText(paths);
             }
         }
-
     }
 }
